07aae039e13405269a83bb769d70dc3de87d07be
Hamish Hossack <hamishhossack@hotmail.co.uk>
2016-02-21 16:43:13 +0000

    Remvoe cache

--- cache/compiled/files/3cc6c6ab98a56ea381cac9f9f5fc602c.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/hamishhossack/Sites/grav/user/config/media.yaml',
-    'modified' => 1455659312,
-    'data' => [
-        
-    ]
-];
